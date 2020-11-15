b43d5dcb531c53cbf5a08af8b5e6132426ea01c8
getuliobr <getulioregis@alunos.utfpr.edu.br>
2020-11-15 20:48:51 -0300

    arrumei dependente com mesmo cpf

--- inserir.sql
+++ inserir.sql
@@ -1,10 +1,7 @@
-DELETE FROM TURNO WHERE id >= 0;
+/* DELETE FROM TURNO WHERE id >= 0;
 DELETE FROM FUNCIONARIO WHERE cpf >= 0;
 DELETE FROM TRABALHA_EM_UM WHERE id >= 0;
-DELETE FROM DEPENDENTE WHERE cpfPai >= 0;
-
-
--- mais funcionarios para serem entregadores
+DELETE FROM DEPENDENTE WHERE cpfPai >= 0; */
 
 INSERT INTO TURNO(id, inicio, fim, diaSemana) VALUES (0, '00:00', '08:00', "Segunda"); 
 INSERT INTO TURNO(id, inicio, fim, diaSemana) VALUES (1, '08:00', '16:00', "Segunda");
@@ -144,8 +141,8 @@ INSERT INTO DEPENDENTE(cpfPai, cpf, nome, dataNasc) VALUES("97246412018", "98182
 INSERT INTO DEPENDENTE(cpfPai, cpf, nome, dataNasc) VALUES("13400701033", "89902131000", "Kelly Bacelar Jardim", '2013-03-02');
 INSERT INTO DEPENDENTE(cpfPai, cpf, nome, dataNasc) VALUES("87030576071", "96038121021", "Charlotte Rico Sabala", '2007-03-11');
 INSERT INTO DEPENDENTE(cpfPai, cpf, nome, dataNasc) VALUES("19902570038", "54222093033", "Jonata Neres Eiró", '2011-06-11');
-INSERT INTO DEPENDENTE(cpfPai, cpf, nome, dataNasc) VALUES("19902570038", "54222093033", "Rahyssa Marroquim Sanches", '2008-05-12');
-INSERT INTO DEPENDENTE(cpfPai, cpf, nome, dataNasc) VALUES("19902570038", "54222093033", "Zhen Tedim Reino", '2008-05-12');
+INSERT INTO DEPENDENTE(cpfPai, cpf, nome, dataNasc) VALUES("19902570038", "46206628019", "Rahyssa Marroquim Sanches", '2008-05-12');
+INSERT INTO DEPENDENTE(cpfPai, cpf, nome, dataNasc) VALUES("19902570038", "24912077020", "Zhen Tedim Reino", '2008-05-12');
 INSERT INTO DEPENDENTE(cpfPai, cpf, nome, dataNasc) VALUES("00563807040", "09549341054", "Nuno Gomes Salvado", '2015-09-26');
 INSERT INTO DEPENDENTE(cpfPai, cpf, nome, dataNasc) VALUES("00563807040", "09549341054", "Lavínia Marcondes Silvaa", '2009-08-07');
 INSERT INTO DEPENDENTE(cpfPai, cpf, nome, dataNasc) VALUES("27510474086", "42166146040", "Telma Borja Azambuja", '2012-04-04');
