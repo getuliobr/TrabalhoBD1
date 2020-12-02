1eb6a939c1b1ab6a52996304a4b520de901ffa8b
getuliobr <getulioregis@alunos.utfpr.edu.br>
2020-12-02 18:47:23 -0300

    todas as consultas prontas

--- consultas.sql
+++ consultas.sql
@@ -15,12 +15,12 @@ WHERE cpf in (
  Media da quantidade produtos por compra
 */
 
-SELECT AVG(t.produtoEmCompra) as produtosPorPedido
+SELECT AVG(temp.produtoEmCompra) as produtosPorPedido
 FROM (
   SELECT COUNT(codigoProduto) as produtoEmCompra
   FROM TEM
   GROUP BY codigoProduto
-) t;
+) temp;
 
 /*
  Quantidade de dependentes de cada funcionario que tem dependentes
@@ -36,29 +36,67 @@ ORDER BY dependentes DESC;
  Quantidade de produtos que cada fornecedor fornece
 */
 
-SELECT f.cnpj, f.nome, temp.produtosFornecidos
-FROM FORNECEDOR f, (SELECT cnpjFornecedor as cnpj, count(cnpjFornecedor) as produtosFornecidos
-FROM PRODUTO
-GROUP BY cnpjFornecedor) temp
-WHERE f.cnpj = temp.cnpj;
+SELECT f.cnpj, f.nome, count(p.cnpjFornecedor) as produtosFornecidos
+FROM FORNECEDOR f, PRODUTO p
+WHERE f.cnpj = p.cnpjFornecedor
+GROUP BY p.cnpjFornecedor;
 
 /*
  Quantidade de funcionario que trabalha em cada turno
 */
 
-SELECT t.diaSemana as dia, t.inicio, t.fim, temp.funcionarios
-FROM TURNO t, (SELECT id, count(id) as funcionarios
-FROM TRABALHA_EM_UM
-GROUP BY id) temp
-WHERE temp.id = t.id;
+SELECT t.diaSemana as dia, t.inicio, t.fim, count(tem.id) as funcionarios
+FROM TURNO t, TRABALHA_EM_UM tem
+WHERE tem.id = t.id
+GROUP BY tem.id;
 
 /*
  Quantidade de entregas feitas por entregador
 */
 
-SELECT f.nome, temp.entregas as entregasFeitas
-FROM FUNCIONARIO f, (SELECT p.cpfEntregador, COUNT(p.cpfEntregador) as entregas
-FROM PEDIDO p
+SELECT f.nome,  COUNT(p.cpfEntregador) as atendidos
+FROM FUNCIONARIO f, PEDIDO p
+WHERE f.cpf = p.cpfEntregador
 GROUP BY p.cpfEntregador
-HAVING p.cpfEntregador) temp
-WHERE f.cpf = temp.cpfEntregador;
+HAVING p.cpfEntregador;
+
+/*
+ Quantos clientes foram atendidos pelos atendentes
+*/
+
+SELECT f.nome as atendente,  COUNT(a.cpfAtendente) as atendidos
+FROM ATENDE a, FUNCIONARIO f
+WHERE f.cpf = a.cpfAtendente
+GROUP BY a.cpfAtendente;
+
+/*
+ Turnos que cada funcionario trabalha
+*/
+
+SELECT f.nome, count(tem.cpf) as turnos
+FROM FUNCIONARIO f, TRABALHA_EM_UM tem
+WHERE f.cpf = tem.cpf
+GROUP BY tem.cpf
+ORDER BY turnos ASC;
+
+/*
+ Pegar o pedido mais antigo de cada estado
+*/
+
+SELECT p.numero as nroPedido, c.nome as cliente, p.estado, p.data, MAX(timestampdiff(day, p.data, curdate())) as diasDesdePedido
+FROM CLIENTE c, PEDIDO p
+WHERE c.cpf = p.cpfCliente
+GROUP BY p.estado
+ORDER BY nroPedido ASC;
+
+/*
+ Pegar o pedido mais recente de todos
+*/
+
+SELECT p.numero as nroPedido, c.nome as cliente, p.estado, p.data
+FROM CLIENTE c, PEDIDO p
+WHERE c.cpf = p.cpfCliente
+AND timestampdiff(day, p.data, curdate()) = (
+	SELECT MIN(timestampdiff(day, ps.data, curdate())) as tempo
+	FROM PEDIDO ps
+)
