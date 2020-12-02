d66ff6faf8481ed175fa90505ce1f4580b0a6556
getuliobr <getulioregis@alunos.utfpr.edu.br>
2020-12-02 12:51:01 -0300

    mais algumas pesquisas

--- consultas.sql
+++ consultas.sql
@@ -20,9 +20,45 @@ FROM (
   SELECT COUNT(codigoProduto) as produtoEmCompra
   FROM TEM
   GROUP BY codigoProduto
-  HAVING COUNT(codigoProduto)
 ) t;
 
 /*
- Quantidade de dependentes cada funcionario tem
+ Quantidade de dependentes de cada funcionario que tem dependentes
 */
+
+SELECT f.nome as funcionario, count(d.cpfPai) as dependentes
+FROM FUNCIONARIO f, DEPENDENTE d
+WHERE f.cpf = d.cpfPai
+GROUP BY d.cpfPai
+ORDER BY dependentes DESC;
+
+/*
+ Quantidade de produtos que cada fornecedor fornece
+*/
+
+SELECT f.cnpj, f.nome, temp.produtosFornecidos
+FROM FORNECEDOR f, (SELECT cnpjFornecedor as cnpj, count(cnpjFornecedor) as produtosFornecidos
+FROM PRODUTO
+GROUP BY cnpjFornecedor) temp
+WHERE f.cnpj = temp.cnpj;
+
+/*
+ Quantidade de funcionario que trabalha em cada turno
+*/
+
+SELECT t.diaSemana as dia, t.inicio, t.fim, temp.funcionarios
+FROM TURNO t, (SELECT id, count(id) as funcionarios
+FROM TRABALHA_EM_UM
+GROUP BY id) temp
+WHERE temp.id = t.id;
+
+/*
+ Quantidade de entregas feitas por entregador
+*/
+
+SELECT f.nome, temp.entregas as entregasFeitas
+FROM FUNCIONARIO f, (SELECT p.cpfEntregador, COUNT(p.cpfEntregador) as entregas
+FROM PEDIDO p
+GROUP BY p.cpfEntregador
+HAVING p.cpfEntregador) temp
+WHERE f.cpf = temp.cpfEntregador;
